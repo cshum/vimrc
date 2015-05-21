e4cce622c809589feeed3b2ed0620438b7db49f1
Adrian Shum <me@cshum.com>
2015-05-21 22:29:19 +0800

    tweaks

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "source $PWD/vimrc.vim" >> ~/.vimrc
-echo 'noremap <leader><leader> :lcd '$(dirname $PWD) >> ~/.vimrc
+echo 'noremap <leader><leader> :lcd '$(dirname $PWD)'/' >> ~/.vimrc
 
 vim +PluginInstall
 
