52ce74f9925383dfbdda19fa55e7b71c2220f325
Adrian Shum <me@cshum.com>
2015-05-21 22:26:06 +0800

    ADD install script

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+set -e
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "source $(pwd)/vimrc.vim" > ~/.vimrc
+
+echo "source $PWD/vimrc.vim" >> ~/.vimrc
+echo 'noremap <leader><leader> :lcd '$(dirname $PWD) >> ~/.vimrc
+
 vim +PluginInstall
 
-#Vim proc
-cd ~/.vim/bundle/vimproc.vim
-make
-
 #YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
+# cd ~/.vim/bundle/YouCompleteMe
+# ./install.sh --clang-completer
 
 #TernForVim
-cd ~/.vim/bundle/tern_for_vim
-npm install
+# cd ~/.vim/bundle/tern_for_vim
+# npm install
 
